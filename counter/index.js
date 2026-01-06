5c1b6c5f8ddd12056fadc7d152b76df2d11fe791
sulemanrajput734-hub <sulemanrajput734@gmail.com>
2026-01-06 19:59:48 +0500

    intial commit

---
+++ counter/index.js
@@ -0,0 +1,25 @@
+function icrement () {
+    var count = document.getElementById("value")
+    var count= count.innerText++;
+     var count = count++
+ 
+   
+}
+
+
+
+
+
+function decrement () {
+      var count = document.getElementById("value")
+var count= count.innerText--;
+ 
+}
+
+function reset () {
+    var count = document.getElementById("value")
+    var count = count.innerText=0
+}
+
+
+
